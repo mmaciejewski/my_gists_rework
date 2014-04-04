e9b1dc975aec6d83f4f8a6464285ec873f917ade
Michał Maciejewski <michal.maciejewski92@gmail.com>
2014-04-04 22:31:05 +0200

    test

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,563 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Gist.create(snippet:
+'public class Java{
+private String abc;
+public Java(String field){
+this.abc = field;
+}
+}', lang: 'java', description: 'java')
+
+Gist.create(snippet:
+'<h1> This is HTML </h1>
+<h2> This is not Java </h2>
+<h3> This is not Javascript"', lang: 'html', description: 'some HTML')
+
+Gist.create(snippet:
+'public class AnotherJavaClass{
+private List<SomeObject> listOfObjects;
+public AnotherJavaClass(String field){
+}
+private void iDoNothingMethod(){
+}
+}', lang: 'java', description: 'Another java')
+
+Gist.create(snippet:
+'echo "My first PHP script!";', lang: 'php', description: 'this is PHP')
+
+Gist.create(snippet:
+'public class Java{
+private String abc;
+public Java(String field){
+this.abc = field;
+}
+}', lang: 'java', description: 'java')
+
+Gist.create(snippet:
+'function myFunction()
+{
+var x="",i;
+for (i=0;i<5;i++)
+{
+x=x + "The number is " + i + "<br>";
+}
+document.getElementById("demo").innerHTML=x;
+}', lang: 'javascript', description: 'javascript')
+
+Gist.create(snippet:
+'<html>
+<head>
+<title>Title of the document</title>
+</head>
+
+<body>
+The content of the document......
+</body>
+
+</html> ', lang: 'html', description: 'html body')
+
+Gist.create(snippet:
+'public class Java{
+private String abc;
+public Java(String field){
+this.abc = field;
+}
+}', lang: 'java', description: 'java')
+
+Gist.create(snippet:
+'echo "My first PHP script!";', lang: 'php', description: 'this is PHP')
+
+Gist.create(snippet:
+'function myFunction()
+{
+return ("Hello world!");
+}', lang: 'javascript', description: 'Javascript function')
+
+Gist.create(snippet:
+'<!DOCTYPE html>
+<html>
+<head>
+<title>Title of the document</title>
+</head>
+
+<body>
+The content of the document......
+</body>
+
+</html> ', lang: 'html', description: 'HTML Declaration')
+
+Gist.create(snippet:
+'public class Java{
+private String abc;
+public Java(String field){
+this.abc = field;
+}
+}', lang: 'java', description: 'java')
+
+Gist.create(snippet:
+"def greet(name):
+print 'Hello', name
+greet('Jack')
+greet('Jill')
+greet('Bob')
+", lang: 'python', description: 'Some python function')
+
+Gist.create(snippet:
+'echo "My first PHP script!";', lang: 'php', description: 'this is PHP')
+
+Gist.create(snippet:
+' var x=Math.PI;
+var y=Math.sqrt(16); ',
+lang: 'javascript', description: 'some Javascript Math')
+
+
+Gist.create(snippet:
+'public class Java{
+private String abc;
+public Java(String field){
+this.abc = field;
+}
+}', lang: 'java', description: 'java')
+
+Gist.create(snippet:
+'<h1> This is HTML </h1>
+<h2> This is not Java </h2>
+<h3> This is not Javascript"', lang: 'html', description: 'some HTML')
+
+Gist.create(snippet:
+'public class AnotherJavaClass{
+private List<SomeObject> listOfObjects;
+public AnotherJavaClass(String field){
+}
+private void iDoNothingMethod(){
+}
+}', lang: 'java', description: 'Another java')
+
+Gist.create(snippet:
+'echo "My first PHP script!";', lang: 'php', description: 'this is PHP')
+
+Gist.create(snippet:
+'public class Java{
+private String abc;
+public Java(String field){
+this.abc = field;
+}
+}', lang: 'java', description: 'java')
+
+Gist.create(snippet:
+'function myFunction()
+{
+var x="",i;
+for (i=0;i<5;i++)
+{
+x=x + "The number is " + i + "<br>";
+}
+document.getElementById("demo").innerHTML=x;
+}', lang: 'javascript', description: 'javascript')
+
+Gist.create(snippet:
+'<html>
+<head>
+<title>Title of the document</title>
+</head>
+
+<body>
+The content of the document......
+</body>
+
+</html> ', lang: 'html', description: 'html body')
+
+Gist.create(snippet:
+'public class Java{
+private String abc;
+public Java(String field){
+this.abc = field;
+}
+}', lang: 'java', description: 'java')
+
+Gist.create(snippet:
+'echo "My first PHP script!";', lang: 'php', description: 'this is PHP')
+
+Gist.create(snippet:
+'function myFunction()
+{
+return ("Hello world!");
+}', lang: 'javascript', description: 'Javascript function')
+
+Gist.create(snippet:
+'<!DOCTYPE html>
+<html>
+<head>
+<title>Title of the document</title>
+</head>
+
+<body>
+The content of the document......
+</body>
+
+</html> ', lang: 'html', description: 'HTML Declaration')
+
+Gist.create(snippet:
+'public class Java{
+private String abc;
+public Java(String field){
+this.abc = field;
+}
+}', lang: 'java', description: 'java')
+
+Gist.create(snippet:
+"def greet(name):
+print 'Hello', name
+greet('Jack')
+greet('Jill')
+greet('Bob')
+", lang: 'python', description: 'Some python function')
+
+Gist.create(snippet:
+'echo "My first PHP script!";', lang: 'php', description: 'this is PHP')
+
+Gist.create(snippet:
+' var x=Math.PI;
+var y=Math.sqrt(16); ',
+lang: 'javascript', description: 'some Javascript Math')
+
+
+
+
+
+
+Gist.create(snippet:
+'public class Java{
+private String abc;
+public Java(String field){
+this.abc = field;
+}
+}', lang: 'java', description: 'java')
+
+Gist.create(snippet:
+'<h1> This is HTML </h1>
+<h2> This is not Java </h2>
+<h3> This is not Javascript"', lang: 'html', description: 'some HTML')
+
+Gist.create(snippet:
+'public class AnotherJavaClass{
+private List<SomeObject> listOfObjects;
+public AnotherJavaClass(String field){
+}
+private void iDoNothingMethod(){
+}
+}', lang: 'java', description: 'Another java')
+
+Gist.create(snippet:
+'echo "My first PHP script!";', lang: 'php', description: 'this is PHP')
+
+Gist.create(snippet:
+'public class Java{
+private String abc;
+public Java(String field){
+this.abc = field;
+}
+}', lang: 'java', description: 'java')
+
+Gist.create(snippet:
+'function myFunction()
+{
+var x="",i;
+for (i=0;i<5;i++)
+{
+x=x + "The number is " + i + "<br>";
+}
+document.getElementById("demo").innerHTML=x;
+}', lang: 'javascript', description: 'javascript')
+
+Gist.create(snippet:
+'<html>
+<head>
+<title>Title of the document</title>
+</head>
+
+<body>
+The content of the document......
+</body>
+
+</html> ', lang: 'html', description: 'html body')
+
+Gist.create(snippet:
+'public class Java{
+private String abc;
+public Java(String field){
+this.abc = field;
+}
+}', lang: 'java', description: 'java')
+
+Gist.create(snippet:
+'echo "My first PHP script!";', lang: 'php', description: 'this is PHP')
+
+Gist.create(snippet:
+'function myFunction()
+{
+return ("Hello world!");
+}', lang: 'javascript', description: 'Javascript function')
+
+Gist.create(snippet:
+'<!DOCTYPE html>
+<html>
+<head>
+<title>Title of the document</title>
+</head>
+
+<body>
+The content of the document......
+</body>
+
+</html> ', lang: 'html', description: 'HTML Declaration')
+
+Gist.create(snippet:
+'public class Java{
+private String abc;
+public Java(String field){
+this.abc = field;
+}
+}', lang: 'java', description: 'java')
+
+Gist.create(snippet:
+"def greet(name):
+print 'Hello', name
+greet('Jack')
+greet('Jill')
+greet('Bob')
+", lang: 'python', description: 'Some python function')
+
+Gist.create(snippet:
+'echo "My first PHP script!";', lang: 'php', description: 'this is PHP')
+
+Gist.create(snippet:
+' var x=Math.PI;
+var y=Math.sqrt(16); ',
+lang: 'javascript', description: 'some Javascript Math')
+
+
+Gist.create(snippet:
+'public class Java{
+private String abc;
+public Java(String field){
+this.abc = field;
+}
+}', lang: 'java', description: 'java')
+
+Gist.create(snippet:
+'<h1> This is HTML </h1>
+<h2> This is not Java </h2>
+<h3> This is not Javascript"', lang: 'html', description: 'some HTML')
+
+Gist.create(snippet:
+'public class AnotherJavaClass{
+private List<SomeObject> listOfObjects;
+public AnotherJavaClass(String field){
+}
+private void iDoNothingMethod(){
+}
+}', lang: 'java', description: 'Another java')
+
+Gist.create(snippet:
+'echo "My first PHP script!";', lang: 'php', description: 'this is PHP')
+
+Gist.create(snippet:
+'public class Java{
+private String abc;
+public Java(String field){
+this.abc = field;
+}
+}', lang: 'java', description: 'java')
+
+Gist.create(snippet:
+'function myFunction()
+{
+var x="",i;
+for (i=0;i<5;i++)
+{
+x=x + "The number is " + i + "<br>";
+}
+document.getElementById("demo").innerHTML=x;
+}', lang: 'javascript', description: 'javascript')
+
+Gist.create(snippet:
+'<html>
+<head>
+<title>Title of the document</title>
+</head>
+
+<body>
+The content of the document......
+</body>
+
+</html> ', lang: 'html', description: 'html body')
+
+Gist.create(snippet:
+'public class Java{
+private String abc;
+public Java(String field){
+this.abc = field;
+}
+}', lang: 'java', description: 'java')
+
+Gist.create(snippet:
+'echo "My first PHP script!";', lang: 'php', description: 'this is PHP')
+
+Gist.create(snippet:
+'function myFunction()
+{
+return ("Hello world!");
+}', lang: 'javascript', description: 'Javascript function')
+
+Gist.create(snippet:
+'<!DOCTYPE html>
+<html>
+<head>
+<title>Title of the document</title>
+</head>
+
+<body>
+The content of the document......
+</body>
+
+</html> ', lang: 'html', description: 'HTML Declaration')
+
+Gist.create(snippet:
+'public class Java{
+private String abc;
+public Java(String field){
+this.abc = field;
+}
+}', lang: 'java', description: 'java')
+
+Gist.create(snippet:
+"def greet(name):
+print 'Hello', name
+greet('Jack')
+greet('Jill')
+greet('Bob')
+", lang: 'python', description: 'Some python function')
+
+Gist.create(snippet:
+'echo "My first PHP script!";', lang: 'php', description: 'this is PHP')
+
+Gist.create(snippet:
+' var x=Math.PI;
+var y=Math.sqrt(16); ',
+lang: 'javascript', description: 'some Javascript Math')
+
+Gist.create(snippet:
+'public class Java{
+private String abc;
+public Java(String field){
+this.abc = field;
+}
+}', lang: 'java', description: 'java')
+
+Gist.create(snippet:
+'<h1> This is HTML </h1>
+<h2> This is not Java </h2>
+<h3> This is not Javascript"', lang: 'html', description: 'some HTML')
+
+Gist.create(snippet:
+'public class AnotherJavaClass{
+private List<SomeObject> listOfObjects;
+public AnotherJavaClass(String field){
+}
+private void iDoNothingMethod(){
+}
+}', lang: 'java', description: 'Another java')
+
+Gist.create(snippet:
+'echo "My first PHP script!";', lang: 'php', description: 'this is PHP')
+
+Gist.create(snippet:
+'public class Java{
+private String abc;
+public Java(String field){
+this.abc = field;
+}
+}', lang: 'java', description: 'java')
+
+Gist.create(snippet:
+'function myFunction()
+{
+var x="",i;
+for (i=0;i<5;i++)
+{
+x=x + "The number is " + i + "<br>";
+}
+document.getElementById("demo").innerHTML=x;
+}', lang: 'javascript', description: 'javascript')
+
+Gist.create(snippet:
+'<html>
+<head>
+<title>Title of the document</title>
+</head>
+
+<body>
+The content of the document......
+</body>
+
+</html> ', lang: 'html', description: 'html body')
+
+Gist.create(snippet:
+'public class Java{
+private String abc;
+public Java(String field){
+this.abc = field;
+}
+}', lang: 'java', description: 'java')
+
+Gist.create(snippet:
+'echo "My first PHP script!";', lang: 'php', description: 'this is PHP')
+
+Gist.create(snippet:
+'function myFunction()
+{
+return ("Hello world!");
+}', lang: 'javascript', description: 'Javascript function')
+
+Gist.create(snippet:
+'<!DOCTYPE html>
+<html>
+<head>
+<title>Title of the document</title>
+</head>
+
+<body>
+The content of the document......
+</body>
+
+</html> ', lang: 'html', description: 'HTML Declaration')
+
+Gist.create(snippet:
+'public class Java{
+private String abc;
+public Java(String field){
+this.abc = field;
+}
+}', lang: 'java', description: 'java')
+
+Gist.create(snippet:
+"def greet(name):
+print 'Hello', name
+greet('Jack')
+greet('Jill')
+greet('Bob')
+", lang: 'python', description: 'Some python function')
+
+Gist.create(snippet:
+'echo "My first PHP script!";', lang: 'php', description: 'this is PHP')
+
+Gist.create(snippet:
+' var x=Math.PI;
+var y=Math.sqrt(16); ',
+lang: 'javascript', description: 'some Javascript Math')
+
+
+
